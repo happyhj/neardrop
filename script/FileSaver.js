28966bcfa06b91b7f703ef7e4aded647e5d9eed7
FourwingsY <fourwingsy@gmail.com>
2015-01-29 04:05:53 +0900

    edit little

--- script/FileSaver.js
+++ script/FileSaver.js
@@ -47,7 +47,6 @@ FileSaver.prototype.initListeners = function() {
 		if(this.blockTranferContext.receivedBlockCount === this.blockTranferContext.totalBlockCount) {
 			console.log("쳥크 다바다따!!: "+Date.now());
 			this.downloadFile();
-			this.emit('transferEnd');
 			// this.off("blockSaved", blockSavedCallback);	
 		}
 		// 다 안받았으면 다음 블록을 보내달라고 송신자에게 응답을 보낸다.
