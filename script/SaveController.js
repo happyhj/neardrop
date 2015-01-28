8919025a92f0e3e191bb425a20c7afec7b1f1f0b
FourwingsY <fourwingsy@gmail.com>
2015-01-29 00:05:01 +0900

    try to refac dataController

---
+++ script/SaveController.js
@@ -0,0 +1,112 @@
+function SaveController(args) {
+	if (!(this instanceof SaveController)) return new SaveController(args);
+	EventEmitter.call(this);
+
+	this.connection = args.conn;
+
+	this.init();
+};
+inherits(SaveController, EventEmitter);
+
+SaveController.prototype.init = function() {
+	this.transferStart = null;
+	this.transferEnd = null;
+
+	this.fileSaver = new FileSaver();
+
+	this.connection.on('data', function(message){
+		this._handleMessage(message);
+	}.bind(this));
+
+	this.fileSaver.on('fileSavePrepared', function(fileInfo) {
+		// UI에서 다루도록 이벤트를 상위 계층으로 올린다.
+		console.log("fileSavePrepared triggered");
+		this.emit('fileSavePrepared', fileInfo);
+	}.bind(this));
+
+	this.fileSaver.on('chunkStored', function() {
+		this.connection.send({
+			"kind": "requestChunk"
+		});
+	}.bind(this));
+
+	// 블록을 다 받았을 때
+	this.fileSaver.on('nextBlock', function(blockIdx) {
+		this.requestBlockTransfer(blockIdx);
+		this.emit('updateProgress', this.getProgress());
+	}.bind(this));
+
+	// 전송이 끝났을 때
+	this.fileSaver.on('transferEnd', function() {
+		this.emit('transferEnd');
+		this.fileEntry = null;
+		this.fileSaver = null;
+	}.bind(this));
+};
+
+SaveController.prototype._handleMessage = function(message) {
+	if( message.byteLength !== undefined ) { // ArrayBuffer 가 도착한 것
+		console.log("Received ByteLength : "+message.byteLength);
+		this.fileSaver.saveChunk(message);
+	}
+	else { // JSON이 도착한 것 
+		var kind = message.kind; // chunk, meta, request
+		switch (kind) {
+
+			// 송신자 -> 수신자
+			case "fileInfo": // 송신자가 보낸 파일 정보가 도착했다. 이를 가지고 file saver 를 초기화한다.
+				console.log("[Connection : _handleMessage] incoming message file info");
+				var fileInfo = message.fileInfo;
+				var chunkSize = message.chunkSize;
+				var blockSize = message.blockSize;
+				this.fileSaver.setFile(fileInfo, chunkSize, blockSize);
+				break;
+			case "fileEnd": // 송신자가 더이상 줄 청크가 없다고 한다. 감사의 인사를 보낸다.
+				// 이건 레알 인사치레. 사실 파일만 다 받으면 TransferEnd이벤트가 발생해서
+				// 파일 다운받고 UI 처리하고 다 한다.
+				console.log("thanks");
+				this.sendThanks();
+				break;
+			
+			default:
+				debugger;
+				console.log("what a fuck...");
+				console.log(typeof message);
+				console.log(message);
+				break;
+		};
+	}
+};
+
+// "이제 블록을 보내라"
+SaveController.prototype.requestBlockTransfer = function(blockIdx) {
+	// 첫 수락시엔 App에서 인자 없이 실행하므로
+	if (!blockIdx) {
+		blockIdx = 0;
+		// 어떤 상대방과 연결되었는지를 UI에 이때 전달, 프로그레스 바 생성
+		this.emit('showProgress', this.connection.peer, 'down');
+	}
+	// 현재 받는 블록이 몇 번 블록인지 저장
+	this.fileSaver.blockTranferContext.blockIndex = blockIdx;
+
+	this.connection.send({
+		"kind": "requestBlock",
+		"blockIndex": blockIdx
+	});
+};
+
+SaveController.prototype.sendThanks = function() {
+	this.connection.send({
+		"kind": "thanks"
+	});
+};
+
+SaveController.prototype.getProgress = function() {
+	var context = this.fileSaver.blockTranferContext;
+
+	var chunkCount = context.receivedChunkCount;
+	if (!chunkCount) chunkCount = 0;
+
+	var progress = chunkCount / context.totalChunkCount;
+	return progress;
+};
---
+++ script/SendController.js
@@ -0,0 +1,126 @@
+function SendController(args) {
+	if (!(this instanceof SendController)) return new SendController(args);
+	EventEmitter.call(this);
+
+	this.connection = args.conn;
+	this.CHUNK_SIZE = args.chunkSize;
+	this.BLOCK_SIZE = args.blockSize;
+	this.fileEntry = null;
+
+	this.init();
+}
+inherits(SendController, EventEmitter);
+
+SendController.prototype.init = function() {
+	this.transferStart = null;
+	this.transferEnd = null;
+
+	this.fileSender = new FileSender();
+
+	this.connection.on('data', function(message){
+		this._handleMessage(message);
+	}.bind(this));
+
+	this.fileSender.on('fileSendPrepared', function(fileInfo) {
+		// UI에서 다루도록 이벤트를 상위 계층으로 올린다.
+		this.emit('fileSendPrepared', fileInfo);
+	}.bind(this));
+
+	this.fileSender.on("blockContextInitialized", function() {
+		this.fileSender.sendDataChunk(this.connection);
+	}.bind(this));
+	
+	this.fileSender.on("blockSent", function() {
+		this.emit('updateProgress', this.getProgress());
+	}.bind(this));
+	
+	this.fileSender.on('transferEnd', function() {
+		console.log("fileEnd");
+		this.connection.send({
+			"kind": "fileEnd"
+		});
+	}.bind(this));
+};
+
+SendController.prototype.sendFile = function(file) {
+	// 수신자에게 파일 정보를 전달
+	this.fileEntry = file;
+	this.askOpponent(file);
+	// 전송자는 파일 전달 준비
+	console.log("fileSender setfile");
+	this.fileSender.setFile(file, this.CHUNK_SIZE, this.BLOCK_SIZE);
+};
+
+SendController.prototype._handleMessage = function(message) {
+
+	var kind = message.kind; // chunk, meta, request
+	switch (kind) {
+		case "refusal": // 수신자가 거부하였다. 연결을 종료한다.
+			this.getRefused();
+			break;
+		case "requestBlock":  // 수신자가 i번째 블록을 요청했다. 이를 통해 현재 블록전송 콘텍스트를 초기화 한다.
+			console.log("[Connection : _handleMessage] incoming message requestBlock");
+			console.log("blockIndex : " + message.blockIndex);	
+			var blockIndex = message.blockIndex;
+			
+			// 블록을 메모리에 로딩 및 청킹
+			this.fileSender.initBlockContext(blockIndex);
+			// 만약 첫 요청이었다면
+			if(blockIndex === 0) {
+				// 어떤 상대방과 연결되었는지 정보를 UI에 이때 전달
+				this.emit('showProgress', this.connection.peer, 'up');
+				// 속도 계산용 기록
+				this.transferStart = Date.now();
+			}
+			break;
+		case "requestChunk": // 수신자가 청크를 잘 받았다. 다음 청크를 보낸다.
+			//console.log("[Connection : _handleMessage] incoming message requestChunk");
+			this.fileSender.sendDataChunk(this.connection);
+			break;
+		case "thanks": // 볼일이 끝났다. 연결을 끊는다.
+			this.emit('transferEnd');
+			this.fileEntry = null;
+			this.fileSender = null;
+			// 송신자가 연결을 끊으면 수신자는 자동적으로 연결이 끊어진다.
+			this.connection.close();
+			break;
+		
+		default:
+			console.error("Unrecognizable Message");
+			console.log(typeof message);
+			console.log(message);
+			break;
+	};
+};
+
+SendController.prototype.askOpponent = function(file) {
+	console.log("메타 정보 보내기");
+	var message = {
+		  "kind": "fileInfo"
+		, "fileInfo": {
+			"lastModifiedDate": file.lastModifiedDate,
+			"name": file.name,
+			"size": file.size,
+			"type": file.type
+		}
+		, "chunkSize": this.CHUNK_SIZE
+		, "blockSize": this.BLOCK_SIZE
+	};
+	this.connection.send(message);
+};
+
+SendController.prototype.getRefused = function() {
+	// TODO: 거절되었다는 메시지를 띄워준다
+	console.log("당신은 바람맞았습니다.");
+	this.connection.close();
+};
+
+SendController.prototype.getProgress = function() {
+	var context = this.fileSender.blockTranferContext;
+
+	var chunkCount = context.sentChunkCount;
+	if (!chunkCount) chunkCount = 0;
+
+	var progress = chunkCount / context.totalChunkCount;
+	return progress;
+};
